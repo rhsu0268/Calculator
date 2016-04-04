256a02f19b94bb2a3040c206c90624fdb6a766ad
Richard Hsu <rhsu0268@gmail.com>
2016-04-04 16:57:59 -0400

    Test

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+    @IBOutlet weak var display: UILabel!
+    var userIsInTheMiddleOfTypingNumber: Bool = false
+    
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        
+        // unwrap the optional with !
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingNumber
+        {
+            display.text = display.text! + digit
+        }
+        else
+        {
+            display.text = digit
+            userIsInTheMiddleOfTypingNumber = true
+        }
+        
+        // optional: unset (nil), and value
+        // type: What type is it set to? - optional String. 
+        // optional that can be a string
+        print("digit = \(digit)")
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
+    
+    
 
 }
 
